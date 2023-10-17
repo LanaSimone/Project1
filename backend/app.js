1aa2c44e8afe2cf91ef039e48f6b4f49027b2fa0
LanaSimone <allanadecarishs@gmail.com>
2023-10-16 21:00:03 -0400

    completed phase 1 authme

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,13 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+const csrfProtection = csurf({ cookie: true });
 
+// Attach the csrfProtection middleware to a route
+app.get('/some/route', csrfProtection, (req, res) => {
+  // Send the CSRF token to the client
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 // Security Middleware
 if (!isProduction) {
@@ -40,6 +46,16 @@ if (!isProduction) {
         sameSite: isProduction && "Lax",
         httpOnly: true
       }
+    })
+  );
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        connectSrc: ["'self'", 'http://localhost:8000'], // Allow connections to 'self'
+        // Add more directives as needed
+      },
     })
   );
 
